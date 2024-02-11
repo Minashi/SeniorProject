eb5aa23799db8ec297c3376f7569e4fb1c1bced2
Brandon Gonzalez <me@brandongonzalez.net>
2024-02-11 05:38:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ ascii_art = """
 \_______/
 """
 
+# MENU OPTIONS
 def menu_option_1():
     print("Access Point Enumeration Here")
 
@@ -33,22 +34,38 @@ def menu_option_2():
 
 def menu_option_3():
     print("Crack WEP Access Points")
-
 # More functions can be added here as needed.
+
+# PIVOT MENU OPTIONS
+def submenu_option_1():
+    print("List Cracked Passwords")
+
+def submenu_option_2():
+    print("Connect To Accesspoint using specified access point")
+    
+def submenu_option_3():
+    print("Enumerate LAN")
+# More functions can be added here as needed.
+
+# NAVIGATION
+def menu_to_submenu():
+    print("Entering Main menu...")
+    display_submenu()
 
 def exit_menu():
     print("Exiting menu...")
 
-# Define menu options here. Add new options by appending to this list.
-menu_options = [
-    {"label": "Option 1", "function": menu_option_1},
-    {"label": "Option 2", "function": menu_option_2},
-    {"label": "Option 3", "function": menu_option_3},
-    # Add new options in this format.
-    {"label": "Exit", "function": exit_menu},
-]
-
 def display_menu():
+    # Define menu options here. Add new options by appending to this list.
+    menu_options = [
+        {"label": "Access Point Enumeration", "function": menu_option_1},
+        {"label": "List Discovered Access Points", "function": menu_option_2},
+        {"label": "Crack WEP", "function": menu_option_3},
+        {"label": "Pivot", "function": menu_to_submenu},
+        # Add new options in this format.
+        {"label": "Exit", "function": exit_menu},
+    ]
+    
     while True:
         print("\nMenu:")
         for i, option in enumerate(menu_options, start=1):
@@ -67,6 +84,27 @@ def display_menu():
         except KeyboardInterrupt:
             print("\nExiting due to Ctrl+C")
             break
+
+def display_submenu():
+    submenu_options = [
+        {"label": "List Cracked Passwords", "function": submenu_option_1},
+        {"label": "Pivot To Access Point", "function": submenu_option_2},
+        {"label": "Enumerate LAN", "function": submenu_option_3},
+        {"label": "Exit Pivot Menu", "function": exit_menu},
+    ]
+    
+    while True:
+        print("\nPivot:")
+        for i, option in enumerate(submenu_options, start=1):
+            print(f"{i}. {option['label']}")
+        
+        choice = int(input("Select an option: "))
+        if 1 <= choice <= len(submenu_options):
+            submenu_options[choice - 1]["function"]()
+            if submenu_options[choice - 1]["label"] == "Exit Pivot Menu":
+                break
+        else:
+            print("Invalid option, please try again.")
 
 # Check if the user is root before proceeding
 check_root_user()
