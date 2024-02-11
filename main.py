ca68ca930782813dfd5a6a86b1e947508db53d46
Brandon Gonzalez <me@brandongonzalez.net>
2024-02-11 04:13:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
+import time
+import sys
+import os
+
+def check_root_user():
+    if os.geteuid() != 0:
+        print("This script must be run as root. Exiting.")
+        sys.exit(1)
+
+def loading_prompt(message="", art=None):
+    if art:
+        print(art)
+    print(message)
+    for _ in range(3):  # Simple loading animation
+        print(".", end="", flush=True)
+        time.sleep(1)
+    print("\n")
+
+# Start up ASCII Art
+ascii_art = """
+   ___  
+  /   \\\\
+ /     \\\\
+/_______\\\\
+\_______/
+"""
+
 def menu_option_1():
-    print("You selected option 1.")
+    print("Access Point Enumeration Here")
 
 def menu_option_2():
-    print("You selected option 2.")
+    print("List out discovered Access Points")
+
+def menu_option_3():
+    print("Crack WEP Access Points")
 
 # More functions can be added here as needed.
 
 def exit_menu():
     print("Exiting menu...")
 
-# Define menu options here.
+# Define menu options here. Add new options by appending to this list.
 menu_options = [
     {"label": "Option 1", "function": menu_option_1},
     {"label": "Option 2", "function": menu_option_2},
+    {"label": "Option 3", "function": menu_option_3},
     # Add new options in this format.
     {"label": "Exit", "function": exit_menu},
 ]
@@ -37,5 +68,9 @@ def display_menu():
             print("\nExiting due to Ctrl+C")
             break
 
-# Run the menu
+# Check if the user is root before proceeding
+check_root_user()
+
+# Initiate loading menu
+loading_prompt("Initializing menu", ascii_art)
 display_menu()
