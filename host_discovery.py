d7aaafa085fef87cef08763681314e21f7faf393
w-machina <157521335+w-machina@users.noreply.github.com>
2024-02-12 23:58:48 -0500

    active host discovery
    uses nmap -sn to discover active hosts on the LAN.

---
+++ host_discovery.py
@@ -0,0 +1,24 @@
+import subprocess
+import argparse
+# DISCLAIMER: This uses NMAP, use on networks you have authorization to test on.
+
+# option -aH performs nmap's -sn target ip. Produces active host IP and MAC ADDRESS.
+
+network = "0"
+def perform_network_scan(network):
+    nmap_command = f'nmap -sn {network}'
+    # airodump-ng for access point enumeration
+    result = subprocess.run(nmap_command, shell=True, capture_output=True, text=True)
+    print(result.stdout)
+
+
+pars = argparse.ArgumentParser(description="Active Host Discovery")
+pars.add_argument("-aH", "--activehosts", required=True, help="Use CIDR notation: 192.168.1.0/24)")
+args = pars.parse_args()
+activehosts = args.activehosts
+
+# user must execute: python active_host_discovery.py -aH <192.168.1.0/24>, change ip for the network you want.
+perform_network_scan(activehosts)
+
+
+
