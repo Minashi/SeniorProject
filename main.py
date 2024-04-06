7b90658dcc282bbc66c6867dfc325727c6f2571e
Brandon Gonzalez <me@brandongonzalez.net>
2024-04-06 07:38:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -341,6 +341,7 @@ def attacking_wpa2():
         {"label": "Identify Potential Clients", "function": wpa2.identify_clients},
         {"label": "Set Target Client", "function": target_client},
         {"label": "Deauth Client", "function": wpa2.deauth},
+        {"label": "Crack WPA2 Handshake", "function": wpa2.crack_handshake},
         {"label": "Return To Main Menu", "function": exit_menu},
     ]
 
