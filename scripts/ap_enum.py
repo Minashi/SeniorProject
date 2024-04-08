21c2e8d6cdc8403ec0cb2b70c3df00230318361a
Brandon Gonzalez <me@brandongonzalez.net>
2024-04-08 02:29:34 -0400

    Update ap_enum.py

--- scripts/ap_enum.py
+++ scripts/ap_enum.py
@@ -1,4 +1,5 @@
 import csv, subprocess, os
+from prettytable import PrettyTable
 
 def ensure_data_directory_exists():
     directory = "/mnt/data"
@@ -80,6 +81,26 @@ def ask_for_scan(interface):
         run_airodump(interface)
     else:
         print("Exiting without scanning.")
+
+def discovered_ap():
+    file_path = "/mnt/data/access_points.txt"
+    # Initialize the PrettyTable with the specified headers
+    table = PrettyTable(["ESSID", "ENC", "BSSID"])
+
+    try:
+        # Open the file and read the contents using the csv.reader for parsing CSV formatted data
+        with open(file_path, mode='r') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)  # Skip the header row
+            for row in csv_reader:
+                bssid, enc, essid = row  # Unpack the row directly into variables
+                table.add_row([essid, enc, bssid])  # Add the data to the table
+
+        print(table)  # Display the table
+    except FileNotFoundError:
+        print(f"The file {file_path} was not found.")
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
 
 def main():
     interface = 'wlan0'
